76d582a8a82fa9330caa39640a5239914689863e
Dawid Wojciechowski <de.wojciechowski@gmail.com>
2021-03-22 14:17:28 +0100

    correction

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const hamBtn = document.querySelector('.hamburger');
-const linksList = document.querySelector('nav ul')
+const linksList = document.querySelector('header ul')
 
 hamBtn.addEventListener('click', function() {
   linksList.classList.toggle('active');
